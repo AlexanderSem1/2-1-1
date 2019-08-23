d084ed911ee4a3d1523b12f0a75f5a3fbf7b9a7c
bunniicat <nina.nny97@gmail.com>
2019-08-23 15:18:45 +0200

    function to make the cards

--- script.js
+++ script.js
@@ -4,4 +4,23 @@ fetch ("https://pokeapi.co/api/v2/pokemon")
 })
 .then (function(response){
     console.log(response);
+    readCards(response);
+    console.log(response.results[0]);
 })
+
+function readCards(response) {
+    for(var i = 0; i < response.results.length; i++) {
+        let cardsContainer = document.getElementById("parentElement");  
+        let card = document.createElement("div");
+        let cardName = document.createElement("h1");
+        let cardButton = document.createElement("a");
+        cardsContainer.appendChild(card);
+        card.appendChild(cardName);
+        card.appendChild(cardButton);
+        cardName.innerHTML = response.results[i].name;
+        cardButton.innerHTML = "View more";  
+    }
+} 
+
+
+
