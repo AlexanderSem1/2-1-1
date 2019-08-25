dfe9c8350e4e88698780c6f5daba4ccd2e1ee62f
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2019-08-25 14:12:06 +0200

    working changing banner

--- banner.js
+++ banner.js
@@ -1,18 +1,24 @@
+
+
 var listArray = [
-    { name: 'Jynx', image: 'images/Lynx.png' }, 
+    { name: 'Jynx', image: 'images/Jynx.png' }, 
     { name: 'Magmar', image: 'images/Magmar.png' }, 
     { name: 'Pikachu', image: 'images/Pikachu.jpg' },
     { name: 'Togepi', image: 'images/Togepi.jpeg' }
   ];
 
+  var index = 1;
+
+  setInterval(function(){ 
+    index == listArray.length ? index = 0 : ""; 
+    /* if index is equal to the length of listArray; 
+    index should be equal to 0. 
+    If else; do nothing */
+    var images = document.getElementById("banner");
+    images.src = listArray[index].image;
+    index++;
+}, 2000);
 
 
 
-for (var i = 0; i < listArray.length; i++){
-    console.log(listArray[i]);
-    setInterval(function(){ 
-        var images = document.getElementById("banner");
-        images.src= listArray.image;
-    }, 3000);
-}
 
