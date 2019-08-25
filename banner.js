47ffd029e528b50ee7d5642b414d4e23d4a03efd
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2019-08-25 18:11:34 +0200

    banner working

---
+++ banner.js
@@ -0,0 +1,24 @@
+
+
+var listArray = [
+    { name: 'Jynx', image: 'images/Jynx.png' }, 
+    { name: 'Magmar', image: 'images/Magmar.png' }, 
+    { name: 'Pikachu', image: 'images/Pikachu.jpg' },
+    { name: 'Togepi', image: 'images/Togepi.jpeg' }
+  ];
+
+  var index = 0;
+
+  setInterval(function(){ 
+    index == listArray.length ? index = 0 : ""; 
+    /* if index is equal to the length of listArray; 
+    index should be equal to 0. 
+    If else; do nothing */
+    var images = document.getElementById("banner");
+    images.src = listArray[index].image;
+    index++;
+}, 2000);
+
+
+
+
